d1fe8ffef5a89970043b6489e45d5c6a08c8482a
sandjian <107805130+sandjian@users.noreply.github.com>
2024-07-05 01:18:20 -0300

    paquetes faltantes

--- app/(routes)/companies/[companyId]/NewContact/FormContact/FormContact.tsx
+++ app/(routes)/companies/[companyId]/NewContact/FormContact/FormContact.tsx
@@ -1,12 +1,10 @@
 "use client";
-
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { Button } from "@/components/ui/button";
 import { Form,FormControl,FormField,FormItem,FormLabel,FormMessage } from "@/components/ui/form";
 import { Select,SelectContent,SelectItem,SelectTrigger,SelectValue } from "@/components/ui/select";
 import { Input } from "@/components/ui/input";
-import { Toast } from "@/components/ui/toast";
 import  axios  from "axios";
 import { useParams,useRouter } from "next/navigation";
 import { FormContactProps } from "./FormContact.types";
